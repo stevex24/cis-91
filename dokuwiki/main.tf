0c880a080342ef34a42c5cb209388c7f3ea0cced
Steve <sdc.stevex@gmail.com>
2021-10-29 04:51:32 +0000

    commmit project 1 progress

--- dokuwiki/main.tf
+++ dokuwiki/main.tf
@@ -31,13 +31,56 @@ provider "google" {
   project     = var.project
 }
 
+
 resource "google_compute_network" "vpc_network" {
   name = "cis91-network"
 }
 
+resource "google_service_account" "proj1-service-account" {
+  account_id   = "proj1-service-account"
+  display_name = "proj1-service-account"
+  description  = "Service account for project1"
+}
+
+resource "google_project_iam_member" "project_member" {
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.proj1-service-account.email}"
+}
+
+resource "google_compute_disk" "system" {
+  name = "system"
+  type = "pd-ssd"
+  zone = var.zone
+}
+
+resource "google_compute_disk" "data" {
+  name = "data"
+  type = "pd-ssd"
+  zone = var.zone
+}
+
+resource "google_storage_bucket" "steves-dokuwiki" {
+  name          = "steves_prime-564819669946735512444543556507"
+}  
+  
 resource "google_compute_instance" "vm_instance" {
   name         = "cis91"
   machine_type = "e2-micro"
+
+  service_account {
+    email  = google_service_account.proj1-service-account.email
+    scopes = ["cloud-platform"]
+  }
+
+  attached_disk {
+    source      = google_compute_disk.data.name
+    device_name = "data"
+  }
+
+  attached_disk {
+    source      = google_compute_disk.system.name
+    device_name = "system"
+  }  
 
   boot_disk {
     initialize_params {
@@ -50,7 +93,7 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
-}
+}  
 
 resource "google_compute_firewall" "default-firewall" {
   name    = "default-firewall"
