50ae79a7b096518aa9c0d9ff728e5c0cc586b9a0
Steve <sdc.stevex@gmail.com>
2021-11-12 00:13:22 +0000

    Committing lab 13

---
+++ lab13/main.tf
@@ -0,0 +1,131 @@
+
+variable "credentials_file" { 
+  default = "../secrets/cis-91.key" 
+}
+
+variable "project" {
+  default = "cis-91-terraform-326400"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  region  = var.region
+  zone    = var.zone 
+  project = var.project
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "cis91-network"
+}
+
+resource "google_compute_instance" "webservers" {
+  count        = 3
+  name         = "web${count.index}"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  labels = {
+    role: "web"
+  }
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name = "default-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance_group" "webservers" {
+  name        = "cis91-webservers"
+  description = "Webserver instance group"
+
+  instances = google_compute_instance.webservers[*].self_link
+
+  named_port {
+    name = "http"
+    port = "80"
+  }
+}
+
+resource "google_compute_health_check" "webservers" {
+  name = "webserver-health-check"
+
+  timeout_sec        = 1
+  check_interval_sec = 1
+
+  http_health_check {
+    port = 80
+  }
+}
+
+resource "google_compute_backend_service" "webservice" {
+  name      = "web-service"
+  port_name = "http"
+  protocol  = "HTTP"
+
+  backend {
+    group = google_compute_instance_group.webservers.id
+  }
+}
+
+
+resource "google_compute_url_map" "default" {
+  name            = "my-site"
+  default_service = google_compute_backend_service.webservice.id
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name     = "web-proxy"
+  url_map  = google_compute_url_map.default.id
+}
+
+resource "google_compute_global_address" "default" {
+  name = "external-address"
+}
+
+#Global
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "forward-application"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.default.id
+  ip_address            = google_compute_global_address.default.address
+}
+
+
+output "lb-ip" {
+  value = google_compute_global_address.default.address
+}
